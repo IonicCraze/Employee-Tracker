acb2d65507f8041e5d5f9825bef1519c94724acd
IonicCraze <daviddeitz97@gmail.com>
2022-06-13 16:37:44 -0400

    added ability to add employee, department, and role

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const inquirer = require('inquirer')
 const mysql = require('mysql2');
 
+
 const app = express();
 
 const db = mysql.createConnection(
@@ -92,6 +93,104 @@ const viewRoles = () => {
     start(); 
   })
 }
+
+const addEmployee = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'firstName',
+      message: 'Enter first name'
+    },
+    {
+      type: 'input',
+      name: 'lastName',
+      message: 'Enter last name'
+    },
+    {
+      type: 'input',
+      name: 'roleid',
+      message: 'Enter roleid'
+    },
+    {
+      type: 'input',
+      name: 'managerid',
+      message: 'Enter managerid'
+    }
+  ])
+  .then((data) => {
+    db.query(`INSERT INTO employee(first_name, last_name, rold_id, manager_id)
+              VALUES('${data.firstName}', '${data.lastName}', ${data.roleid}, ${data.managerid})`);
+    
+    db.query(`SELECT * FROM employee`, (err, res) => {
+      if (err) {
+        console.log(err);
+        return 'error'
+      }
+      console.table(res);
+    });
+  
+    start();
+  });
+}
+
+const addDepartment = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'departmentName',
+      message: 'Enter department name'
+    }
+  ])
+  .then((data) => {
+    db.query(`INSERT INTO departments(department_name)
+              VALUES('${data.departmentName}')`);
+    
+    db.query(`SELECT * FROM departments`, (err, res) => {
+      if (err) {
+        console.log(err);
+        return 'error'
+      }
+      console.table(res);
+    });
+  
+    start();
+  });
+}
+
+const addRole = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Enter role name'
+    },
+    {
+      type: 'input',
+      name: 'roleSalary',
+      message: 'Enter role salary'
+    },
+    {
+      type: 'input',
+      name: 'departmentid',
+      message: 'Enter department id'
+    }
+  ])
+  .then((data) => {
+    db.query(`INSERT INTO roles(title, salary, department_id)
+              VALUES('${data.title}', '${data.roleSalary}', '${data.departmentid}')`);
+    
+    db.query(`SELECT * FROM roles`, (err, res) => {
+      if (err) {
+        console.log(err);
+        return 'error'
+      }
+      console.table(res);
+    });
+  
+    start();
+  });
+}
+
 
 start();
 
